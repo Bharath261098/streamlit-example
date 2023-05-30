f301ab826c5a26c151a2ac66021b64e5ae9f31ce
Bharath <bharathboss68367@gmail.com>
2023-05-30 14:48:56 +0530

    Updated sidebar heading

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ image = Image.open('exl.png')
 
 with st.sidebar:
     st.image(image, width = 150)
-    st.header('Conversational BI')
+    st.header('Call Summarizier')
 
 def generate_summary(text):
     prompt = "summarize the conversation provided and also suggest the next action item for the Customer and Executive:\n\n"
