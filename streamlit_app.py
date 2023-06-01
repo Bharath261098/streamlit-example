ffb37740cc42baafddcaadeb3fc50cf68398faa8
Bharath <bharathboss68367@gmail.com>
2023-06-02 01:03:45 +0530

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,50 @@
+import openai
+import streamlit as st
+from pandasai import PandasAI
+from pandasai.llm.openai import OpenAI
+from PIL import Image
+
+# Configure OpenAI API
+llm = OpenAI(api_token=st.secrets["chat_gpt_key"])
+pandas_ai = PandasAI(llm, conversational=False)
+
+image = Image.open('exl.png')
+
+with st.sidebar:
+    st.image(image, width = 150)
+    st.header('Service Call Summarizer')
+
+def generate_summary(text):
+    prompt = "summarize the conversation in bullet points and also suggest one action item for the Customer and the Executive:\n\n"
+    prompt += text
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        max_tokens=100,
+        temperature=0.5,
+        n=1,
+        stop=None,
+        top_p=None,
+        frequency_penalty=0.2,
+        presence_penalty=0.8
+    )
+    summary = response.choices[0].text.strip()
+    summary = summary.replace("Summarized Conversation:\n\n", "")  # Remove the title
+    return summary
+
+def get_next_action_items(summary):
+    customer_action_items = []
+    executive_action_item = ""
+    sentences = summary.split('. ')
+    for sentence in sentences:
+        sentence = sentence.strip()
+        if 'customer' in sentence.lower():
+            if not sentence.startswith("Summarized Conversation:"):
+                customer_action_items.append(sentence)
+        if 'executive' in sentence.lower():
+            executive_action_item = sentence
+    return customer_action_items, executive_action_item
+
 def main():
     st.title('Service Call Summarizer')
     st.write('Upload text files to generate a summary and identify the next action items.')
