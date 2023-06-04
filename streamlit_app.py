d4af0107049bbbb56572d63bead5de4ddf212f35
Bharath261098 <105351804+Bharath261098@users.noreply.github.com>
2023-06-05 04:43:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -109,16 +109,19 @@ def main():
 
         if page == 'claims':
             show_claims_page()
+        elif page == 'View Claims':
+        # Clear the summary placeholder to hide the summary
+        summary_placeholder.empty()
 
-def show_claims_page():
-    st.title('Claims')
-    claims_table = """| Date       | Customer name | Claim Type         | Claim Description                     | Action item                              | Claim amount | Claim status |
+        st.title('Claims')
+        claims_table = """| Date       | Customer name | Claim Type         | Claim Description                     | Action item                              | Claim amount | Claim status |
 |------------|---------------|--------------------|---------------------------------------|-----------------------------------------|--------------|--------------|
 | 02/02/2023 | John Doe      | Health Insurance   | Medical expenses reimbursement        | Raise a claim request with insurance provider | $10,000      | Approved     |
 | 04/02/2023 | John Doe      | Health Insurance   | Medical expenses reimbursement        | Follow up with insurance provider             | $10,000      | In progress  |
 | 06/02/2023 | John Doeh     | Health Insurance   | Medical expenses reimbursement        | Review settlement details                    | $10,000      | Settled      |
 """
-    st.markdown(claims_table)
+        st.markdown(claims_table)
+
 
 if __name__ == '__main__':
     main()
