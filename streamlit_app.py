ba4513e0a2e4798024bb72b2260e4bc0a150884b
Bharath261098 <105351804+Bharath261098@users.noreply.github.com>
2023-06-05 04:15:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ pandas_ai = PandasAI(llm, conversational=False)
 image = Image.open('exl.png')
 
 with st.sidebar:
-    st.image(image, width = 150)
+    st.image(image, width=150)
     st.header('Service Call Summarizer')
 
 def generate_summary(text):
@@ -54,14 +54,6 @@ def extract_claim_number(file_name):
         return int(match.group(1))
     return 0
 
-import re
-
-def extract_claim_number(file_name):
-    match = re.search(r'claim(\d+)', file_name)
-    if match:
-        return int(match.group(1))
-    return 0
-
 def main():
     st.title('Service Call Summarizer')
     st.write('Upload text files to generate a summary and identify the next action items.')
@@ -92,6 +84,23 @@ def main():
             st.write(f'- {executive_action_item}')
         else:
             st.write('No executive action item identified.')
+
+        # Static summary information below upload button
+        st.header('Previous Summary Information')
+        st.write('04/02/2023 - 
+
+        Customer inquires about the status of their insurance claim.
+        Executive checks the claim status and informs the customer that it has been approved and the settlement amount will be sent via the payment method specified in their policy.
+        Customer confirms the payment method and when they can expect to receive the settlement amount.
+        Executive confirms that the payment will be deposited into their bank account within five business days and suggests reviewing the settlement details
+
+        02/02/2023 - 
+
+        Customer is raising an insurance claim for a recent car accident.
+        Executive verifies the policy details and requests additional information about the incident.
+        Customer provides the additional information, including date and time of the accident, a description of what happened, and if there were any injuries involved.
+        Executive initiates the claim process and informs the customer that they may need to provide supporting documents.
+        Customer agrees')
 
 if __name__ == '__main__':
     main()
